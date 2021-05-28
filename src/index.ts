b560d483d5febe25f9b0542f3ece3fa860deb395
Stone Tao <stonezt2019@gmail.com>
2021-05-27 23:11:44 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line
-import * as types from './RL/utils/types';
+import * as types from './RL/utils/types'; // import this first so other modules have access to some global typings
 export * as Environments from './RL/Environments';
 export * as Spaces from './RL/Spaces';
 export { DP } from './RL/DP';

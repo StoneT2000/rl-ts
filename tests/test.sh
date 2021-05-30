ef087ed8df5f7bd67292cc6d840104fbd2c0abf2
Stone Tao <stonezt2019@gmail.com>
2021-05-30 02:37:43 -0700

    Update test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# ts-node "$SCRIPT_DIR/utils/clusterTools/testscript.ts"
-
 mocha --recursive 'tests/**/*.spec.ts'
+
+ts-node "$SCRIPT_DIR/utils/clusterTools/testscript.ts"

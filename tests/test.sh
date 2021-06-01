607d508f4004a0db45c4886a3e7350255abe96f0
Stone Tao <stonezt2019@gmail.com>
2021-05-31 19:40:11 -0700

    fix bugs

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-mocha -r ts-node/register -r tsconfig-paths/register --recursive 'tests/**/*.spec.ts'
+nyc --reporter=json mocha -r ts-node/register -r tsconfig-paths/register --recursive 'tests/**/*.spec.ts'
 
 ts-node "tests/utils/clusterTools/testscript.ts"

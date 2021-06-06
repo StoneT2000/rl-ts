6e19214b5d0c8accdb58b4e89ba4d6668e9b0e76
Stone Tao <stonezt2019@gmail.com>
2021-06-06 01:17:36 -0700

    Update test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-nyc --reporter=json mocha -r ts-node/register -r tsconfig-paths/register --recursive 'tests/**/*.spec.ts'
-
-ts-node "tests/utils/clusterTools/testscript.ts"
+nyc --reporter=json mocha -r ts-node/register -r tsconfig-paths/register --recursive 'tests/**/*.spec.ts' && ts-node "tests/utils/clusterTools/testscript.ts"
